eb520152cce71d2c60aef8ff34f1f86d670d9da7
dothanhlam97 <dothanhlam.98@gmail.com>
2017-11-25 12:36:15 +0700

    add template website

---
+++ shop-master/sw-precache-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  staticFileGlobs: [
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-loader.js',
+    'images/*'
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'webcomponentsjs-polyfills-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/images\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
+};
