3ac08ffe2fba1039835827018d2aa652bc33b3e3
dothanhlam97 <dothanhlam.98@gmail.com>
2017-11-26 10:22:20 +0700

    I love phuong thao

---
+++ WeShoppy/omoshiroilib/src/main/java/com/martin/ads/omoshiroilib/flyu/hardcode/DemoConstants.java
@@ -0,0 +1,25 @@
+package com.martin.ads.omoshiroilib.flyu.hardcode;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public class DemoConstants {
+    public static final String SDCARD = getSDPath();
+    public static final String APPDIR = SDCARD + "/Omoshiroi/TestFaceU";
+    public static final String IMAGE_PATH="image_path";
+
+    public static String getSDPath() {
+        File sdDir = null;
+        // 判断 SD 卡是否存在
+        boolean sdCardExist = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        if (sdCardExist) {
+            sdDir = Environment.getExternalStorageDirectory(); // 获取根目录
+        }
+        if (sdDir != null) {
+            return sdDir.toString();
+        } else {
+            return "";
+        }
+    }
+}
